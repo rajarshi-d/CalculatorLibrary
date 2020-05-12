a7d722815133113721eace2b553a4881a66656c7
Rajarshi <rajarshi.das@in.ibm.com>
2020-05-12 23:07:52 +0530

    test module updated

--- calculator.py
+++ calculator.py
@@ -9,12 +9,4 @@ def subtract(first_term, second_term):
     return first_term - second_term
 
 
-def multiply(first_term, second_term):
-    return first_term * second_term
 
-
-def divide(first_term, second_term):
-    if second_term == 0:
-        return "Nice try!"
-    else:
-        return first_term/second_term
